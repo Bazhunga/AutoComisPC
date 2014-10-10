801d32fe1a563604189fc8ae7542f8eaa60e4b60
Bazhunga <zhu.s.kevin@gmail.com>
2014-10-10 00:18:46 -0400

    Final

--- AC.lua
+++ AC.lua
@@ -1,5 +1,9 @@
+--require "os"
 scriptId = 'com.AutoComis.examples.myFirstScript'
 
+function onForegroundWindowChange(app, title) --This is a callback defined by Myo Script
+	return true
+end
 --Unlock Mechanism
 function unlock()
 	unlocked = true
@@ -11,8 +15,19 @@ function extendUnlock()
 end
 
 function onPoseEdge(pose, edge)
+	myo.debug(pose .. ": " .. edge)
+
+	--Message Sending
+    if unlocked and pose ~= "rest" then
+    	if edge == "off" then
+    		sendMsg (pose, edge) 
+    	end
+    end
     -- Unlock
-    if pose == "thumbToPinky" then
+    if pose == "fist" then
+    	if unlocked == false then
+    		myo.debug("Unlocked!")
+    	end
         if edge == "off" then
             -- Actually unlock when pose is released in case the user holds it for a
             -- while.
@@ -25,21 +40,72 @@ function onPoseEdge(pose, edge)
             extendUnlock()
         end
     end
-    if unlocked
-    	if pose = "fist"
-    		myo.debug("You see this pose? It's the screw you pose!")
-    		myo.vibrate("short")
 
-    	if pose = "fingersSpread"
-    		myo.debug("This pose is extremely apologetic! I'm so sorry...")
-    		myo
+    
+   	
+end
 
-    	if pose = "waveIn"
-    		myo.debug("This pose indicates that I want to turn left :)")
+function onPeriodic()
+	--Locking
+	if unlockedSince ~= nil then
+	    if myo.getTimeMilliseconds() - unlockedSince >= 2000 and unlocked == true then
+	    	lock()
+	    	myo.vibrate("short")
+	    end
+   	end
+end
+function lock ()
+	myo.debug("Locked!")
+	unlocked = false
+end
 
-    	if pose = "waveOut"
-    		myo.debug("I WANNA TURN RIGHT YO BEEP BEEP")
-    end
+function sendMsg (pose, edge)
+	unlockedSince = myo.getTimeMilliseconds()
 
+	if pose == "fist" then
+		myo.debug("You see this pose? It's the screw you pose!")
+		myo.keyboard("a", "press")
+		myo.keyboard("return","press")
+	end
+
+	--[[if pose == "fingersSpread" then
+		myo.debug("This pose is extremely apologetic! I'm so sorry...")
+		myo.keyboard("c", "press")
+		myo.keyboard("return","press")
+	end	]]
+
+	if pose == "waveIn" then
+		--if myo.getArm() == "left" then
+			--myo.debug ("I WANN TURN RIGHT YO BEEP BEEP")
+			--myo.keyboard("b", "press")
+			--myo.keyboard("return","press")
+		--else
+			--myo.debug("This pose indicates that I want to turn left :)")
+			--extendUnlock()
+		--end
+		myo.debug("This pose indicates that I want to turn left :)")
+		myo.keyboard("b", "press")
+		myo.keyboard("return","press")
+
+	end
+
+	if pose == "waveOut" then
+		--if myo.getArm() == "left" then
+			--myo.debug("This pose indicates that I want to turn left :)")
+			--myo.keyboard("b", "press")
+			--myo.keyboard("return","press")
+
+
+		--else
+			--myo.debug ("I WANN TURN RIGHT YO BEEP BEEP")
+
+		--end
+		myo.debug("This pose indicates that I want to turn left :)")
+		myo.keyboard("b", "press")
+		myo.keyboard("return","press")
+
+	end
+
+	myo.vibrate("short")
 end
 
